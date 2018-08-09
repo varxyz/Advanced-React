89a63a743e63f27b2e5752d5b7753796e7c53496
varxyz <mrsergiumoraru@gmail.com>
2018-08-09 12:55:44 +0300

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+
+module.exports = {
+  entry: ['babel-polyfill', './lib/components/index.js'],
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: 'babel-loader',
+        exclude: /node_modules/
+      }
+    ]
+  }
+};
